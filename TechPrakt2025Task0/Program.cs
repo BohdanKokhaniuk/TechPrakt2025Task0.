508193f844086a1338248577142beb42659e5300
BohdanKohanyuk <ipz234_kbb@student.ztu.edu.ua>
2025-02-19 15:43:06 +0200

    Додав основні файли проекту

---
+++ TechPrakt2025Task0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть перше число: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введіть друге число: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int sum = num1 + num2;
+        Console.WriteLine($"Сума чисел: {sum}");
+    }
+}
